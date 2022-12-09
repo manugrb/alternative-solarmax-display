2efd09f3cdf23d371eed43fb19ef57334a5a2d45
Manuel Groebl <m.groebl@online.de>
2022-12-09 14:10:29 +0100

    Create getInverterJson function

---
+++ backend/inverterInterface.js
@@ -0,0 +1,41 @@
+import getRawInverterData from './datafetcher.js';
+
+function getInverterJson(){
+
+    const inverterJsonPromise = new Promise((resolve, reject) => {
+
+        getRawInverterData().then((value) => {
+
+            const rawData = value;
+            const rawDataPieces = rawData.split("%");
+
+            const solarPower = rawDataPieces[1];
+            const housePowerUsage = rawDataPieces[3];
+            const gridPower = rawDataPieces[4];
+            const batteryPower = rawDataPieces[5];
+            const batteryCharge = rawDataPieces[6];
+            const batteryCapacity = rawDataPieces[7];
+
+            const inverterJson = {
+                "solarPower": solarPower,
+                "housePower": housePowerUsage,
+                "gridPower": gridPower,
+                "batteryPower": batteryPower,
+                "batteryCharge": batteryCharge,
+                "batteryCapacity": batteryCapacity
+            }
+
+            resolve(inverterJson);
+
+        });
+    
+
+    });
+
+    return inverterJsonPromise;
+
+}
+
+getInverterJson().then((value) => {
+    console.log(value);
+});
