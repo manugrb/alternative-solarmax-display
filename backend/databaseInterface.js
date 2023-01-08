6a9e75a65c0754767148954b608af2bd71abfe7d
Manuel Groebl <m.groebl@online.de>
2023-01-09 00:08:11 +0100

    Prepare databaseInterface to do queries

--- backend/databaseInterface.js
+++ backend/databaseInterface.js
@@ -1,1 +1,30 @@
-const mysql = require('mysql');
+import mysql from "mysql";
+import dotenv from "dotenv";
+dotenv.config();
+
+const databaseHost = process.env.DATABASEHOST;
+const databaseUser = process.env.DATABASEUSER;
+const password = process.env.PASSWORD;
+const databaseName = process.env.DATABASE;
+
+const con = mysql.createConnection({
+    host: databaseHost,
+    user: databaseUser,
+    password: password
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    const createDBSQL = "CREATE DATABASE IF NOT EXISTS " + databaseName + ";";
+    con.query(createDBSQL, function (err, result) {
+      if (err) throw err;
+      const useDBSQL = "use " + databaseName + ";"
+      con.query(useDBSQL, function (err, result) {
+          if (err) throw err;
+          console.log('worked');
+      });
+    });
+
+   
+});
