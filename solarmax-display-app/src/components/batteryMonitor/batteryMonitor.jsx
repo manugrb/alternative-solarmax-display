9d87b8856e62c3316bbdc128e37c8969e152e05a
Manuel Groebl <m.groebl@online.de>
2023-01-08 22:16:44 +0100

    Make batteryMonitor work with actual data

--- solarmax-display-app/src/components/batteryMonitor/batteryMonitor.jsx
+++ solarmax-display-app/src/components/batteryMonitor/batteryMonitor.jsx
@@ -4,18 +4,26 @@ import '../../constants.css';
 
 const BatteryMonitor = (props) => {
 
+
+    function getBodyInfill(batteryFill){
+
+        if(batteryFill >= 0.95) return "100%";
+
+        const fill = (batteryFill / 0.95) * 100;
+        return fill + "%";
+
+    }
+
     return(
         <div className="batteryMonitorWrapper">
 
             <div className="batteryBodyOutline">
-                <div className="batteryInfill">
+                <div className="batteryInfill" style={{width: getBodyInfill(props.batteryFill)}}>
 
                 </div>
             </div>
             <div className="batteryPoleOutline">
-                <div className="batteryPoleInfill">
 
-                </div>
             </div>
 
         </div>
