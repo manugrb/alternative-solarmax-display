857597dd7a96de2c7c07c87a76c4ea0965735ca2
Manuel Groebl <m.groebl@online.de>
2023-01-09 18:53:30 +0100

    Create setup function for sql to avoid nested code

--- backend/databaseInterface.js
+++ backend/databaseInterface.js
@@ -2,10 +2,12 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
+const inverterDataTableName = "inverterData";
+
 const databaseHost = process.env.DATABASEHOST;
 const databaseUser = process.env.DATABASEUSER;
 const password = process.env.PASSWORD;
-const databaseName = process.env.DATABASE;
+const databaseName = process.env.DATABASENAME;
 
 const con = mysql.createConnection({
     host: databaseHost,
@@ -16,15 +18,47 @@ const con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    const createDBSQL = "CREATE DATABASE IF NOT EXISTS " + databaseName + ";";
-    con.query(createDBSQL, function (err, result) {
-      if (err) throw err;
-      const useDBSQL = "use " + databaseName + ";"
-      con.query(useDBSQL, function (err, result) {
-          if (err) throw err;
-          console.log('worked');
-      });
+    setupDatabaseConnection().then(() => {
+        console.log("setup complete");
+    }, (reason) => {
+        console.error(reason);
+    })
+    
+});
+
+function setupDatabaseConnection(){
+
+    const setupPromise = new Promise((resolve, reject) => {
+
+        const createDBSQL = "CREATE DATABASE IF NOT EXISTS " + databaseName + ";";
+        con.query(createDBSQL, function (err, result) {
+            if (err){
+                reject(err);
+                throw err;
+            }
+            const useDBSQL = "use " + databaseName + ";"
+            con.query(useDBSQL, function (err, result) {
+                if (err){
+                    reject(err);
+                    throw err;
+                }                
+              
+                const createTableSQL = "CREATE TABLE IF NOT EXISTS " + inverterDataTableName + " (time TIMESTAMP, solarPower SMALLINT UNSIGNED, housePower SMALLINT, gridPower SMALLINT, batteryPower SMALLINT, batteryCharge SMALLINT UNSIGNED, batteryCapacity SMALLINT);";
+                con.query(createTableSQL, function(err, result){
+                    if(err){
+                        reject(err);
+                        throw err;
+                    }
+
+                    resolve();
+
+                });
+
+          });
+        });
+
     });
 
-   
-});
+    return setupPromise;
+
+}
